25bcffacf88a21b47f84654da1708578234aff62
Cheezy Amuzus <37597167+CheezyAmuzus@users.noreply.github.com>
2019-07-23 01:19:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
 // This will check if the node version you are running is the required
 // Node version, if it isn't it will throw the following error to inform
 // you.
