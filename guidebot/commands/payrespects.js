fe04cdbd71f730b603ebe60cc25d0b261ee1f14d
Cheezy Amuzus <cheezyhaxx@gmail.com>
2019-07-22 21:41:31 -0700

    New Update GuideBot

---
+++ guidebot/commands/payrespects.js
@@ -0,0 +1,17 @@
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+    message.channel.send('**F**');
+  };
+  
+  exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: "User"
+  };
+  
+  exports.help = {
+    name: "payrespects",
+    category: "Fun",
+    description: "Yoshi will pay his respects.",
+    usage: "payrespects"
+  };
